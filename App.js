ff3a34a2f285c24369c40b43c6fefbad12abd29e
Ikeya <yuumake@gmail.com>
2021-10-23 21:26:23 +1100

    added new features

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, SafeAreaView, TextInput } from 'react-native';
+import Constants from 'expo-constants'
+import {Item} from './components/Item'
+
 
 export default function App() {
+
+  const [data, setData] = useState()
+
+  const AppData = [
+    {id: "1", name: "Apple"},
+    {id: "2", name: "Orange"},
+    {id: "3", name: "Strawberry"},
+    {id: "4", name: "Pear"},
+    {id: "5", name: "Banana"},
+  ]
+
+  const Renderer = ({item}) => (<Item text={item.name} />)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TextInput style={styles.input} />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Add to list</Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList data={AppData} keyExtractor={(item) => item.id} renderItem={Renderer} />
     </View>
   );
 }
@@ -14,8 +35,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'lightyellow',
+    marginTop: Constants.statusBarHeight,
+  },
+  header:{
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  input: {
+    backgroundColor:'white',
+    fontSize: 20,
+    borderColor: '#dddddd',
+    borderWidth: 1,
+    padding: 5,
+    flex: 1,
+  },
+  button:{
+    backgroundColor: 'black',
+  },
+  buttonText:{
+    color: "white",
+    padding: 10,
   },
 });
